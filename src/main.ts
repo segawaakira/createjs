86eb62232083b35a90efc253fdcb57961db18396
segawa <>
2019-07-01 01:13:17 +0900

    時計を出すところまで

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import MyAsset = lib.MyAsset;
 import {LoadAsset} from "./load-asset";
 
 var stage = null;
+var clock = null;
 var asset:MyAsset = null;
 var asset2:MyAsset = null;
 
@@ -13,6 +14,7 @@ window.addEventListener("load", function () {
 function startCreateJS() {
   // Stageオブジェクトを作成します。表示リストのルートになります。
   stage = new createjs.Stage("myCanvas");
+  clock = new createjs.Stage("clock");
   asset = new MyAsset();
   asset2 = new MyAsset();
 
@@ -27,10 +29,39 @@ function startCreateJS() {
  // createjs.Ticker.addEventListener("tick", handleTick);
   asset2.y += 250;
   stage.update();
+  createjs.Ticker.addEventListener("tick", time);
 }
 
 function handleTick() {
   // 描画を更新する
   stage.update();
   asset2.y += 100;
+}
+
+
+
+function time() {
+  // // Stageオブジェクトを作成。表示リストのルートになります。
+  // var stage = new createjs.Stage("myCanvas");
+  // // Text インスタンスを作成
+  var label = new createjs.Text("", "20px sans-serif", "red");
+  // Text インスタンスは一度だけしか stage に追加しない
+  clock.addChild(label);
+  // 時間経過のイベント
+  createjs.Ticker.addEventListener("tick", handleTick);
+  function handleTick() {
+    // 現在時間を取得
+    var now = new Date();
+    // 時間の数値を取得
+    var h = now.getHours(); // 時(0〜23)
+    var m = now.getMinutes(); // 分(0〜59)
+    var s = now.getSeconds(); // 秒(0〜59)
+    // 表示文言を作成
+    var time = h + ":" + m + ":" + s;
+    // Text インスタンスの文字列を更新
+    label.text = time;
+    // Stageの描画を更新
+    clock.update();
+  }
+
 }
